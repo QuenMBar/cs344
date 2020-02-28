14cdfbe49c929d797b6c80e260c8a2b26ad5b7da
QuenMBar <Qmb2@students.calvin.edu>
2020-02-28 00:12:54 -0500

    HW1 PT3 WIP

--- TempHw.py
+++ TempHw.py
@@ -1,118 +1,100 @@
 """
-This module implements local search on the Traveling Salesman Problem.  It uses
-simulated annealing and hill climbing to do this.  It will randomly create a city
-that has a list of linked cities in it.  It will then use the above algorithms to
-try and find the shortest path through them.
+This uses a pre-built problem specification for the
+Zebra puzzle taken from AIMA's csp.py. See:
 
-@author: Quentin Barnes
+    https://en.wikipedia.org/wiki/Zebra_Puzzle
+
+@author: kvlinden
+@version 14feb2013
 """
-import math
 import time
-from search import Problem, hill_climbing, simulated_annealing, \
-    exp_schedule, genetic_search
-from random import randrange, shuffle
+
+from csp import min_conflicts, backtracking_search, AC3, CSP, parse_neighbors
+from search import depth_first_graph_search
 
 
-class TPSVariant(Problem):
-    """
-    State: Current order of citys
-    Action: Take the city with the longest distance to the next one, then make a list
-    of all the other cities swaped with that one.
-    Value: Total distance took to travel between the cities
-    """
-
-    # Sets the initial state, the size of the city, the configuration of the cities, and the maximum distance
-    def __init__(self, initial, citySize, cityConfig, maximum, connectHome):
-        self.initial = initial[:]
-        self.citySize = citySize
-        self.cityConfig = cityConfig
-        self.maximum = maximum
-        self.connectHome = connectHome
-
-    # It takes the city with the max distance to the next one and then
-    # makes a list of that city swaped with each other city as the actions
-    def actions(self, state):
-        max = self.getMaxDistNode(state)
-        options = []
-        for i in range(self.citySize):
-            if i != max:
-                newOption = state[:]
-                newOption[i], newOption[max] = newOption[max], newOption[i]
-                options.append(newOption[:])
-        return options[:]
-
-    # Result is the action taken
-    def result(self, state, action):
-        return action[:]
-
-    # Inverts the realValue since the algorithms are looking for a maximum,
-    # while we are looking for the minimum distance
-    def value(self, state):
-        return self.maximum - self.realValue(state)
-
-    # Uses the city configuration to sum up the total distance that the salesman travels
-    def realValue(self, state):
-        totalDist = 0
-        for i in range(self.citySize - 1):
-            totalDist += self.cityConfig[state[i]][state[i + 1]]
-        if self.connectHome:
-            totalDist += self.cityConfig[self.citySize-1][1]
-        return totalDist
-
-    # Finds and returns the index of the node with the max distance
-    def getMaxDistNode(self, state):
-        indexOfMax = 0
-        lengthOfMax = 0
-        for n in range(self.citySize - 1):
-            if self.cityConfig[state[n]][state[n + 1]] > lengthOfMax:
-                indexOfMax = n
-                lengthOfMax = self.cityConfig[state[n]][state[n + 1]]
-        return indexOfMax
+def print_solution(result):
+    """A CSP solution printer copied from csp.py."""
+    for h in range(1, 6):
+        print('House', h)
+        for (var, val) in result.items():
+            if val == h:
+                print('\t', var)
 
 
-if __name__ == '__main__':
+def Zebra():
+    """Return an instance of the Zebra Puzzle."""
+    Rooms = 'GoldLab RedLab'.split()
+    Teachers = 'Schuurman Adams Norman VanderLinden'.split()
+    Classes = 'CS108 CS112 CS212 CS214 CS344 CS300 CS384'.split()
+    variables = Rooms + Teachers + Classes
+    domains = {}
+    for var in variables:
+        domains[var] = ['MWF800', 'MWF900', 'MWF1030', 'TTH1030', 'TTH1200']
+    neighbors = parse_neighbors("""Schuurman: CS300;
+                Schuurman: CS384; VanderLinden: CS344; VanderLinden: CS108;
+                Norman: CS214; Adams: CS212; Adams: CS112""", variables)
+    for type in [Rooms, Teachers, Classes]:
+        for A in type:
+            for B in type:
+                if A != B:
+                    if B not in neighbors[A]:
+                        neighbors[A].append(B)
+                    if A not in neighbors[B]:
+                        neighbors[B].append(A)
+    print(neighbors)
+    print(domains)
 
-    # Sets the values of how large the city is and the max distance between them
-    # Also sets if the problem should be a loop or not
-    fillInSize = citySize = 30
-    maxDistance = 20
-    connectHome = False
+    def zebra_constraint(A, a, B, b, recurse=0):
+        return a == b
+        # same = (a == b)
+        # if A == 'Englishman' and B == 'Red':
+        #     return same
+        # if A == 'Spaniard' and B == 'Dog':
+        #     return same
+        # if A == 'Chesterfields' and B == 'Fox':
+        #     return next_to
+        # if A == 'Norwegian' and B == 'Blue':
+        #     return next_to
+        # if A == 'Kools' and B == 'Yellow':
+        #     return same
+        # if A == 'Winston' and B == 'Snails':
+        #     return same
+        # if A == 'LuckyStrike' and B == 'OJ':
+        #     return same
+        # if A == 'Ukranian' and B == 'Tea':
+        #     return same
+        # if A == 'Japanese' and B == 'Parliaments':
+        #     return same
+        # if A == 'Kools' and B == 'Horse':
+        #     return next_to
+        # if A == 'Coffee' and B == 'Green':
+        #     return same
+        # if A == 'Green' and B == 'Ivory':
+        #     return a - 1 == b
+        # if recurse == 0:
+        #     return zebra_constraint(B, b, A, a, 1)
+        # if ((A in Colors and B in Colors) or
+        #         (A in Pets and B in Pets) or
+        #         (A in Drinks and B in Drinks) or
+        #         (A in Countries and B in Countries) or
+        #         (A in Smokes and B in Smokes)):
+        #     return not same
+        # raise Exception('error')
+    return CSP(variables, domains, neighbors, zebra_constraint)
 
-    # Creates the city configuration
-    maximum = citySize * maxDistance
-    cityConfig = [[0 for x in range(citySize)] for y in range(citySize)]
-    for i in range(citySize):
-        fillInSize -= 1
-        for j in range(fillInSize):
-            cityConfig[i + j + 1][i] = cityConfig[i][i +
-                                                     j + 1] = randrange(1, maxDistance, 1)
 
-    # Creates an initial state
-    totalHill = 0
-    totalSim = 0
-    for i in range(100):
-        initial = [x for x in range(citySize)]
-        shuffle(initial)
+puzzle = Zebra()
 
-        p = TPSVariant(initial, citySize, cityConfig, maximum, connectHome)
+# result = depth_first_graph_search(puzzle)
+# result = AC3(puzzle)
+result = backtracking_search(puzzle)
+# result = min_conflicts(puzzle, max_steps=1000)
 
-        time1 = time.time()
-        hill_solution = hill_climbing(p)
-        time2 = time.time()
-
-        simAneal = simulated_annealing(
-            p,
-            exp_schedule(k=20, lam=0.005, limit=1000)
-        )
-        time3 = time.time()
-        totalHill += p.realValue(hill_solution)
-        totalSim += p.realValue(simAneal)
-
-    print(totalHill / 100)
-    print(totalSim/100)
-
-    print("Initial: \t\t\t\t\t", initial, "  Distance: ", p.realValue(initial))
-    print("Hill Climbing solution: \t", hill_solution,
-          "  Distance: ", p.realValue(hill_solution), "  Time: ", (time2 - time1))
-    print("Sim Anneal solution: \t\t", simAneal,
-          "  Distance: ", p.realValue(simAneal), "  Time: ", (time3 - time2))
+if puzzle.goal_test(puzzle.infer_assignment()):
+    print("Solution:\n")
+    print_solution(result)
+else:
+    print("failed...")
+    print(puzzle.curr_domains)
+    puzzle.display(puzzle.infer_assignment())
